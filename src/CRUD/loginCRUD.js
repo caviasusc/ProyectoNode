53dec31b03005cf74700f64d6af927a0976940de
Cesar Viasus <caviasusc@gmail.com>
2021-02-02 18:28:21 -0500

    m

--- src/CRUD/loginCRUD.js
+++ src/CRUD/loginCRUD.js
@@ -1,23 +1,27 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken")
 
-exports.createToken = async(req,res)=>{
-    const user = await User.findOne({ email: req.body.email });
-    const token = jwt.sign({
-        id: user._id,
-        email: user.email
-    },
-        process.env.TOKEN_SECRET
-    )
+exports.createToken = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        const token = jwt.sign({
+            id: user._id,
+            email: user.email
+        },
+            process.env.TOKEN_SECRET
+        )
 
-    res.header('auth-token', token).json({
-        error: null,
-        token: token,
-        name: user.name
-    })
+        res.header('auth-token', token).json({
+            error: null,
+            token: token,
+            name: user.name
+        })
+    } catch (error) {
+        res.status(400).json(error)
+    }
 }
 
-exports.getPayload = async(req, res)=>{
+exports.getPayload = async (req, res) => {
     const token = req.header('auth-token')
     try {
         const verificar = jwt.verify(token, process.env.TOKEN_SECRET)
